ecd565978fd93afebca2bd49d5f423bd43d1de1b
tougshire <tougshire@tougshire.com>
2023-05-17 09:36:38 -0400

    Initial commit

---
+++ tougdo.py
@@ -0,0 +1,104 @@
+from datetime import date, timedelta
+import tkinter as tk
+import re
+window = tk.Tk()
+window.title('Tougshore To Do List')
+
+
+def refresh(txtarea):
+
+    f = open("c:\\users\\benja\\onedrive\\todo_txt\\todo.txt", "r")
+    data = f.read()
+    f.close()
+    items = data.split('\n')
+    parsed_items=[]
+    itempattern = '^\([A-Za-z]\)\s.*'
+    duepattern = 'due:((\d\d\d\d-\d\d-\d\d)|today|tomorrow)'
+    pripattern = '^\([A-Za-z]\)'
+    projpattern = '\+\w*'
+    
+    for item in items:
+        if re.match(itempattern, item):
+            item_text = item
+            parsed_item = {}
+            parsed_item['pri'] = ''
+            primatch=re.search(pripattern, item_text)
+            if primatch:
+                parsed_item['pri'] = primatch[0].upper()
+                item_text = re.sub(pripattern, '', item_text)
+
+            parsed_item['due'] = ''
+            duematch=re.search(duepattern, item_text)
+            if duematch:
+                duedate = duematch[0]
+                if duedate == 'due:today':
+                    duedate = 'due:' + date.today().isoformat()
+                if duedate == 'due:tomorrow':
+                    duedate = 'due:' + (date.today() + timedelta(days=1)).isoformat()
+                parsed_item['due'] = duedate
+                item_text = re.sub(duepattern, '', item_text)
+
+
+            parsed_item['projs'] = []
+            projmatch=re.search(projpattern, item_text)
+            while projmatch:
+                parsed_item['projs'].append(projmatch[0])
+                item_text = re.sub(projpattern, '', item_text)
+                projmatch=re.search(projpattern, item_text)
+
+            parsed_item['text'] = item_text
+
+            parsed_items.append(parsed_item)
+
+
+    parsed_items = sorted(parsed_items, key=lambda i: ( i['due'], i['pri'] ) )
+
+    data=''
+
+    previous_due = ''
+    for parsed_item in parsed_items:
+        if previous_due != parsed_item['due'].strip():
+            data = data + '\n'
+            previous_due = parsed_item['due'].strip()
+
+        data = data + parsed_item['pri'].strip() + ' ' + parsed_item['due'].strip() + ' ' + parsed_item['text'].strip()
+        if parsed_item['projs']:
+            parsed_item['projs'].sort()
+            for proj in parsed_item['projs']:
+                data = data + ' ' + proj
+
+        data = data + '\n' 
+
+    txtarea.delete("1.0", "end")
+    txtarea.insert('1.0',data)
+
+def save(txtarea):
+
+    f = open("c:\\users\\benja\\onedrive\\todo_txt\\todo.txt", "w")
+    itempattern = '^\([A-Z]\)\s.*'
+
+    data = txtarea.get(1.0, tk.END)
+    items = data.split('\n')
+    data = ''
+    for i, item in enumerate(items):
+        if re.match(itempattern, item):
+            data = data + item + '\n'
+    f.write(data)
+    f.close()
+
+    refresh(txtarea)
+
+items = []
+txtarea = tk.Text(window, width=400)
+txtarea.pack(pady=20, expand='yes')
+
+font=('Calibri 35')
+
+refresh_btn=tk.Button(window,height=1,width=10, text="Refresh",command=lambda: refresh(txtarea))
+refresh_btn.pack()
+save_btn=tk.Button(window,height=1,width=10, text="Save",command=lambda: save(txtarea))
+save_btn.pack()
+
+refresh(txtarea)
+
+window.mainloop()
