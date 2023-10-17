73c4ffc05591e3049a17e8eef4ad127a9846a363
tougshire <tougshire@tougshire.com>
2023-10-17 04:57:19 -0400

    Change DateTime fields to Date and add Tag slug

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.utils import timezone
-
+from datetime import date, timedelta
 from django.db import models
 from django.urls import reverse
 
 
 def one_week_hence():
-    return timezone.now() + timezone.timedelta(days=7)
+    return date.today + timedelta(days=7)
 
 
 class Tag(models.Model):
@@ -25,9 +25,9 @@ class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(default=one_week_hence)
+    due_date = models.DateField(default=one_week_hence)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    done_date = models.DateTimeField("is done", blank=True, null=True)
+    done_date = models.DateField("is done", blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse("tougdo:item-update", self.pk)
